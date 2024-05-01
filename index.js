d271ca795cfd5ab46bc476d7436e87b7e413b7a2
Hunter <153857391+7Hunter7@users.noreply.github.com>
2024-05-01 21:10:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,21 +5,23 @@ const port = 3000;
 const fs = require("fs");
 const path = require("path");
 const pathToCounter = path.join(__dirname, "./Counter.json");
-const Count = JSON.parse(fs.readFileSync(pathToCounter, "utf-8"));
+
 
 app.get("/", (req, res) => {
+  const Count = JSON.parse(fs.readFileSync(pathToCounter, "utf-8"));
+  Count.home += 1;
   res.send(
     `<h1>Главная страница</h1><p>Просмотров: ${Count.home}</p><a href="/about">Ссылка на страницу About</a>`
   );
-  Count.home += 1;
   fs.writeFileSync(pathToCounter, JSON.stringify(Count));
 });
 
 app.get("/about", (req, res) => {
+  const Count = JSON.parse(fs.readFileSync(pathToCounter, "utf-8"));
+  Count.about += 1;
   res.send(
     `<h1>Страница About</h1><p>Просмотров: ${Count.about}</p><a href="/">Ссылка на страницу Home</a>`
   );
-  Count.about += 1;
   fs.writeFileSync(pathToCounter, JSON.stringify(Count));
   console.log(Count);
 });
