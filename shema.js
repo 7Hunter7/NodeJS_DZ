e8d18d655ae66177b3ab526807495999a3dfc357
Hunter <153857391+7Hunter7@users.noreply.github.com>
2024-05-02 10:23:41 +0300

    Update shema.js

--- shema.js
+++ shema.js
@@ -1,7 +1,7 @@
 const Joi = require("joi");
 
 const schema = Joi.object({
-  firstName: Joi.string().min(2).required(),
+  firstName: Joi.string().min(1).required(),
   secondName: Joi.string().min(2).required(),
   age: Joi.number().min(0).max(120).required(),
   city: Joi.string().min(2),
